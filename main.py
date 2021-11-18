31355fb354ba69f2de8dd8913eb40d09d12f147c
Larry Salomon <salomonlarry@gmail.com>
2021-11-18 21:48:26 +0100

    Shop Capitalize Liczby podzielne przez 5

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 
-the_shop = {'piekarnia':['Chleb', 'Pączek', 'Bułki'],
-             'warzywniak':['Marchew', 'Seler', 'Rukola']}
+the_shop = {'piekarnia':['chleb', 'pączek', 'bułki'],
+             'warzywniak':['marchew', 'seler', 'rukola']}
 
 summ = 0
 
 print('Lista zakupów')
 for shop, value in the_shop.items():
     summ += len(value)
-    print(f'Idę do {shop.capitalize()} i kupuję tam {value}')
+    print(f'Idę do {shop.capitalize()} i kupuję tam {[item.capitalize() for item in value]}')
 print(f'W sumie kupuję {summ} produktów.')
 
-for i in range(0,101,5):
-    print(i, i**3)
+for i in range(0,101):
+    if i % 5 == 0 and not i == 0:
+        print(i, i**3)
 
