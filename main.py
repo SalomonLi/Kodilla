6cdbde241c739e413a91d00ae7517a1efb4a5ca8
Larry Salomon <salomonlarry@gmail.com>
2021-11-18 21:09:23 +0100

    Shop Capitalize

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 the_shop = {'piekarnia':['Chleb', 'Pączek', 'Bułki'],
              'warzywniak':['Marchew', 'Seler', 'Rukola']}
 
@@ -9,5 +10,6 @@ for shop, value in the_shop.items():
     print(f'Idę do {shop.capitalize()} i kupuję tam {value}')
 print(f'W sumie kupuję {summ} produktów.')
 
-#for i in range(0,101,5):
-#    print(i, i**5)
+for i in range(0,101,5):
+    print(i, i**3)
+
