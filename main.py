cd2a474dae174528cf6e76f300dc98ee5095159b
Larry Salomon <salomonlarry@gmail.com>
2021-11-18 20:43:46 +0100

    old version

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('HelLo World')
+the_shop = {'piekarnia':['Chleb', 'Pączek', 'Bułki'],
+             'warzywniak':['Marchew', 'Seler', 'Rukola']}
+
+summ = 0
+
+print('Lista zakupów')
+for shop, value in the_shop.items():
+    summ += len(value)
+    print(f'Idę do {shop.capitalize()} i kupuję tam {value}')
+print(f'W sumie kupuję {summ} produktów.')
+
+#for i in range(0,101,5):
+#    print(i, i**5)
